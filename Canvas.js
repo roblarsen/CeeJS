ef701386d8a77cc62909531bf6c1ecdaeecd99ea
Bob Holt <BobHolt@gmail.com>
2011-09-20 01:54:47 +0000

    Further work - simple shapes and paths implementation

--- Canvas.js
+++ Canvas.js
@@ -9,6 +9,15 @@
  * Date: Thu Sep 1 17:20:56 2011 -0400
  */
 
+// TODO: I built this object using jQuery as a model just to see how it worked. It doesn't need to stay this way.
+
+// DONE: basic shapes and paths
+// TODO: images - https://developer.mozilla.org/en/Canvas_tutorial/Using_images
+// TODO: styles and colors - https://developer.mozilla.org/en/Canvas_tutorial/Applying_styles_and_colors
+// TODO: transformations - https://developer.mozilla.org/en/Canvas_tutorial/Transformations
+// TODO: compositing - https://developer.mozilla.org/en/Canvas_tutorial/Compositing
+// TODO: animations - https://developer.mozilla.org/en/Canvas_tutorial/Basic_animations
+
 (function(window){
     var document = window.document,
         navigator = window.navigator,
@@ -31,6 +40,7 @@
                     fillStyle = "#000",
                     strokeStyle = "#000",
                     getCurrentPos = function(){
+                        //TODO: Make sure all methods update currentPos where applicable
                         return {
                             x: xCurrentPos,
                             y: yCurrentPos
@@ -52,22 +62,58 @@
                         context.beginPath();
                         return getCurrentPos();
                     },
+                    bezierCurveTo = function(cp1x, cp1y, cp2x, cp2y, x, y){
+                        context.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+                        return getCurrentPos();
+                    },
                     circle = function(params){
                         params = params || {};
                         var x = params.x || xCurrentPos,
                             y = params.y || yCurrentPos,
                             radius = params.radius || 10,
-                            fill = params.fill || false;
+                            fillStyle = params.fillStyle || false;
+                        moveTo(x,y);
+                        beginPath();
                         arc({
                             x: x,
                             y: y,
                             radius: radius
                         });
                         stroke();
-                        if(fill){
+                        if(fillStyle){
+                            context.fillStyle = fillStyle;
                             context.fill();
                         }
                         closePath();
+                        return getCurrentPos();
+                    },
+
+                    /*
+                     * Function: clearRect
+                     *
+                     * Clears a rectangular area, making it fully transparent
+                     *
+                     * Parameters:
+                     *  params.x - (Integer) Starting x coordinate. Defaults to the current position.
+                     *  params.y - (Integer) Starting y coordinate. Defaults to the current position.
+                     *  params.width - (Integer) Rectangle width. Defaults to 0.
+                     *  params.height - (Integer) Rectangle height. Defaults to 0.
+                     *
+                     * Returns:
+                     *  An object containing the current x and y positions.
+                     *
+                     * See Also:
+                     *
+                     *  <circle>
+                     *  <rectangle>
+                     */
+                    clearRect = function(params){
+                        params = params || {};
+                        var x = params.x || xCurrentPos,
+                            y = params.y || yCurrentPos,
+                            width = params.width || 0,
+                            height = params.height || 0;
+                        context.clearRect(x,y,width,height);
                         return getCurrentPos();
                     },
                     closePath = function(){
@@ -79,6 +125,7 @@
                         return getCurrentPos();
                     },
                     lineFor = function(distance,angle){
+                        // TODO: implement
                         return getCurrentPos();
                     },
                     lineTo = function(x,y){
@@ -93,6 +140,57 @@
                       yCurrentPos = y;
                       return getCurrentPos();
                     },
+                    quadraticCurveTo = function(cp1x, cp1y, x, y){
+                        context.quadraticCurveTo(cp1x, cp1y, x, y);
+                        return getCurrentPos();
+                    },
+                    quadraticCurveToFixed = function ( cpx, cpy, x, y ) {  
+                        /* for FF1.5 - from MDN: https://developer.mozilla.org/en/Canvas_tutorial/Drawing_shapes
+                        /* 
+                        For the equations below the following variable name prefixes are used: 
+                            qp0 is the quadratic curve starting point (you must keep this from your last point sent to moveTo(), lineTo(), or bezierCurveTo() ). 
+                            qp1 is the quadratic curve control point (this is the cpx,cpy you would have sent to quadraticCurveTo() ). 
+                            qp2 is the quadratic curve ending point (this is the x,y arguments you would have sent to quadraticCurveTo() ). 
+                        We will convert these points to compute the two needed cubic control points (the starting/ending points are the same for both 
+                        the quadratic and cubic curves. 
+
+                        The exact equations for the two cubic control points are: 
+                            cp0 = qp0 and cp3 = qp2 
+                            cp1 = qp0 + (qp1 - qp0) * ratio 
+                            cp2 = cp1 + (qp2 - qp0) * (1 - ratio) 
+                        where ratio = (sqrt(2) - 1) * 4 / 3 exactly (approx. 0.5522847498307933984022516322796) 
+                        if the quadratic is an approximation of an elliptic arc, and the cubic must approximate the same arc, or 
+                        ratio = 2.0 / 3.0 for keeping the same quadratic curve. 
+
+                        In the code below, we must compute both the x and y terms for each point separately. 
+
+                        cp1x = qp0x + (qp1x - qp0x) * ratio; 
+                        cp1y = qp0y + (qp1y - qp0y) * ratio; 
+                        cp2x = cp1x + (qp2x - qp0x) * (1 - ratio); 
+                        cp2y = cp1y + (qp2y - qp0y) * (1 - ratio); 
+
+                        We will now  
+                            a) replace the qp0x and qp0y variables with currentX and currentY (which *you* must store for each moveTo/lineTo/bezierCurveTo) 
+                            b) replace the qp1x and qp1y variables with cpx and cpy (which we would have passed to quadraticCurveTo) 
+                            c) replace the qp2x and qp2y variables with x and y. 
+                        which leaves us with:  
+                        */  
+                        var ratio = 2.0 / 3.0; // 0.5522847498307933984022516322796 if the Bezier is approximating an elliptic arc with best fitting  
+                        var cp1x = xCurrentPos + (cpx - xCurrentPos) * ratio;  
+                        var cp1y = yCurrentPos + (cpy - yCurrentPos) * ratio;  
+                        var cp2x = cp1x + (x - xCurrentPos) * (1 - ratio);  
+                        var cp2y = cp1y + (y - yCurrentPos) * (1 - ratio);  
+
+                        // and now call cubic Bezier curve to function   
+                        bezierCurveTo( cp1x, cp1y, cp2x, cp2y, x, y );  
+
+                        xCurrentPos = x;  
+                        yCurrentPos = y;  
+                    },
+                    rect = function(x, y, width, height){
+                        context.rect(x, y, width, height);
+                        return getCurrentPos();
+                    },
 
                     /*
                      * Function: rectangle
@@ -104,7 +202,7 @@
                      *  params.y - (Integer) Starting y coordinate. Defaults to the current position.
                      *  params.width - (Integer) Rectangle width. Defaults to 0.
                      *  params.height - (Integer) Rectangle height. Defaults to 0.
-                     *  params.fill - (Boolean) If the rectangle should be filled. Defaults to false.
+                     *  params.fillStyle - (String) The valid fillStyle.
                      *
                      * Returns:
                      *  An object containing the current x and y positions.
@@ -112,6 +210,7 @@
                      * See Also:
                      *
                      *  <circle>
+                     *  <clearRect>
                      */
                     rectangle = function(params){
                         params = params || {};
@@ -119,12 +218,21 @@
                             y = params.y || yCurrentPos,
                             width = params.width || 0,
                             height = params.height || 0,
-                            fill = params.fill || false;
-                        if (fill) {
-                            context.fillRect(x,y,width,height);
+                            fillStyle = params.fillStyle || false;
+                        if (fillStyle) {
+                            context.fillStyle = fillStyle;
+                            fillRect(x,y,width,height);
                         } else {
-                            context.strokeRect(x,y,width,height);
+                            strokeRect(x,y,width,height);
                         }
+                        return getCurrentPos();
+                    },
+                    fillRect = function(x,y,width,height){
+                        context.fillRect(x,y,width,height);
+                        return getCurrentPos();
+                    },
+                    strokeRect = function(x,y,width,height){
+                        context.strokeRect(x,y,width,height);
                         return getCurrentPos();
                     },
 
@@ -142,6 +250,21 @@
                         xCurrentPos = yCurrentPos = 0;
                         return getCurrentPos();
                     },
+                    roundedRectangle = function(x,y,width,height,radius){
+                        // from MDN: https://developer.mozilla.org/en/Canvas_tutorial/Drawing_shapes
+                        beginPath();  
+                        moveTo(x,y+radius);  
+                        lineTo(x,y+height-radius);  
+                        quadraticCurveTo(x,y+height,x+radius,y+height);  
+                        lineTo(x+width-radius,y+height);  
+                        quadraticCurveTo(x+width,y+height,x+width,y+height-radius);  
+                        lineTo(x+width,y+radius);  
+                        quadraticCurveTo(x+width,y,x+width-radius,y);  
+                        lineTo(x+radius,y);  
+                        quadraticCurveTo(x,y,x,y+radius);  
+                        stroke(); 
+                        return getCurrentPos();
+                    },
 
                     stroke = function(){
                         context.stroke();
@@ -149,13 +272,25 @@
                     };
 
                 return {
+                    arc: arc,
+                    beginPath: beginPath,
+                    bezierCurveTo: bezierCurveTo,
                     circle: circle,
+                    clearRect: clearRect,
+                    closePath: closePath,
+                    fill: fill,
+                    fillRect: fillRect,
                     getCurrentPos: getCurrentPos,
                     lineFor: lineFor,
                     lineTo: lineTo,
+                    moveTo: moveTo,
+                    quadraticCurveTo: quadraticCurveTo,
+                    rect: rect,
                     rectangle: rectangle,
                     reset: reset,
-                    stroke: stroke
+                    roundedRect: roundedRectangle,
+                    stroke: stroke,
+                    strokeRect: strokeRect
                 }
             }
         }
@@ -166,7 +301,3 @@
     }());
     window.Canvas = Canvas;
 }(window));
-
-/*
-it should have quadratic curves and bezier curves in it too
-*/
