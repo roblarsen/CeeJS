7d5e73fd786c86211105f525a2e45a25a011476f
Marc Neuwirth <marc.neuwirth@gmail.com>
2011-10-07 01:20:41 +0000

    Added canvas testing script from data-url

---
+++ test/js/script.js
@@ -0,0 +1,60 @@
+//Some shapes dont match up exactly when loading them from a data url, not sure why.
+//An error threshold of 4 seems to do the trick so far
+var ERROR_THRESHOLD = 4;
+
+function startTest(name, drawCanvas, controlImage){
+	var ctx = document.getElementById('ctx'),
+		ctxContext = ctx.getContext('2d'),
+		controlCtx = document.getElementById('control-ctx')
+		controlCtxContext = controlCtx.getContext('2d'),
+		img = new Image();
+	
+	img.src = controlImage;
+	img.onload = function(){
+		drawCanvas('ctx');
+		controlCtxContext.drawImage(img,0,0);
+		
+		imageData = ctxContext.getImageData(0, 0, ctx.width, ctx.height);
+		imageDataControl = controlCtxContext.getImageData(0, 0, controlCtx.width, controlCtx.height);
+		var length = imageData.data.length,
+			count = 0;
+		
+		if(imageDataControl.data.length === length){
+		while(length--){
+			if( imageData.data[length] !== imageDataControl.data[length] &&
+			   Math.abs(imageData.data[length] - imageDataControl.data[length]) > ERROR_THRESHOLD ){
+				
+				var log = 'no match at ' + length + ': ' +  [imageData.data[length], imageDataControl.data[length]],
+					li = document.createElement('li'),
+					ul = document.getElementById('log');
+					
+				li.innerText =  log;
+				ul.appendChild(li);
+				
+				console.log(log);
+				count++;
+				
+				if(count > 2000){
+					console.log('exiting: too many errors');
+					var errorRate = document.getElementById('error-rate');
+					errorRate.innerText = 'too many';
+					return;
+				}
+			}
+		}
+		
+		var errorRate = document.getElementById('error-rate'),
+			rate = count / imageData.data.length * 100 + '%';
+		
+		if(errorRate){
+			errorRate.innerText = rate ;
+		}
+		
+		console.log(name + ' error rate: '  + rate );
+		
+		}
+		else {
+			console.log('different length');
+		}
+	}
+}
